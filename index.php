3224bcf25b5c774a05d7935a5943942dcb12e5a8
poojapauskar <git.poojapauskar@gmail.com>
2016-07-13 17:42:33 +0530

    initial

---
+++ index.php
@@ -0,0 +1,12 @@
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+
+<script>
+        $(document).ready(function (){
+         if(navigator.userAgent.toLowerCase().indexOf("android") > -1){
+             window.location.href = 'https://itunes.apple.com/in/app/beautiful-photos/id1111832526?mt=8';
+         }
+         if(navigator.userAgent.toLowerCase().indexOf("iphone") > -1){
+             window.location.href = 'https://itunes.apple.com/in/app/beautiful-photos/id1111832526?mt=8';
+         }
+        });
+</script>
